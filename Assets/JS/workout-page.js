8f497154fa634c1fa7397b72aeea1eecf93b6632
osorioj <osorioj_88@hotmail.com>
2022-10-17 21:48:15 -0400

    update js

--- Assets/JS/workout-page.js
+++ Assets/JS/workout-page.js
@@ -1,44 +1,47 @@
-var muscleChosen = JSON.parse(localStorage.getItem('Muscle Group'));
-var userName = localStorage.getItem('Full Name');
-var userEmail = localStorage.getItem('Email');
-console.log(muscleChosen);
-console.log(userName);
-console.log(userEmail);
+var musclesChosen = JSON.parse(localStorage.getItem('Muscle Group'));
 
+//muscle variables for api calls
 var apiMuscleList = {
-    Chest: [`pectoralis%20major`],
-    Back: [`latissimus%20dorsi`],
-    Shoulders: [`deltoid`],
-    Biceps: [`biceps`],
-    Triceps: [`triceps`],
-    Legs: [`quadriceps`, `hamstrings`, `gluteus%20maximus`],
-  };
-  console.log(apiMuscleList.Chest);
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "a2e2218b81msh172204e911c7de7p1a8ea0jsnc46101fc6953",
-      "X-RapidAPI-Host": "exerciseapi3.p.rapidapi.com",
-    },
-  };
-  
-  function grabExercises() {
-    for (let i - 0; i < muscleChosen.length; i++) {
-        if (apiMuscleList.muscleChosen[i].length > 0) {
-             })
+  Chest: [`pectoralis%20major`],
+  Back: [`latissimus%20dorsi`],
+  Shoulders: [`deltoid`],
+  Biceps: [`biceps`],
+  Triceps: [`triceps`],
+  Legs: [`quadriceps`, `hamstrings`, `gluteus%20maximus`],
+};
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a2e2218b81msh172204e911c7de7p1a8ea0jsnc46101fc6953",
+    "X-RapidAPI-Host": "exerciseapi3.p.rapidapi.com",
+  },
+};
+
+function grabExercises() {
+  for (let i = 0; i < musclesChosen.length; i++) {
+    if (musclesChosen[i] === `Legs`) {
+      for (let x = 0; x < 3; x++) {
+        fetch(
+          `https://exerciseapi3.p.rapidapi.com/search/?primaryMuscle=${apiMuscleList.Legs[x]}`,
+          options
+        )
+          .then((response) => response.json())
+          .then((response) => console.log(response))
+          .catch((err) => console.error(err));
+      }
+    } else {
+      fetch(
+        `https://exerciseapi3.p.rapidapi.com/search/?primaryMuscle=${
+          apiMuscleList[musclesChosen[i]][0]
+        }`,
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => console.log(response))
+        .catch((err) => console.error(err));
     }
   }
+} 
 
-  fetch(
-    `https://exerciseapi3.p.rapidapi.com/search/?primaryMuscle=${apiMuscleList[muscleChosen[i]][0]}`,
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-  }
-}
-
-grabExercises(muscleChosen);
-console.log(muscleChosen[0]);
-console.log(apiMuscleList[muscleChosen[0]]);
+grabExercises(musclesChosen);
